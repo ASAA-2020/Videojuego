f08846ef10b280e5f7643429307554e1920cbc1a
ASAA-2020 <64322623+ASAA-2020@users.noreply.github.com>
2020-05-21 18:55:04 +0200

    Add files via upload

---
+++ Videojuego4/com/main.js
@@ -0,0 +1,13 @@
+var bWeb;
+console.log('ssssshey');
+
+window.onload = function () {
+
+    if (document.readyState == "complete") {
+        console.log("Hey");
+        bWeb = new clsStoreApp(window, this.document);
+        this.document.getElementById('canvas').addEventListener('click', bWeb.onClick);
+    }
+
+
+}
